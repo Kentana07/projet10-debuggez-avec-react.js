304242c98266237e1a0225f5eac74fde7c4b7a9e
Kellian Barrois <kellian07@gmail.com>
2024-04-25 20:53:17 +0200

    ajout du slider sans bug de défillement

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,17 +5,24 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
+  // Utilisation d'un hook pour récupérer les données nécessaires.
   const { data } = useData();
+  // État local pour suivre l'index de l'élément actuellement affiché.
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+
+  // Tri des événements par date en ordre décroissant.
+  const byDateDesc = data?.focus.sort((evtA, evtB) => 
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
+
+  // Fonction pour passer à la carte suivante avec un intervalle de 5 secondes.
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => {
+      // Met à jour l'index pour montrer la prochaine carte ou retourner au début si c'est la dernière.
+      setIndex(index + 1 < byDateDesc?.length ? index + 1 : 0);
+    }, 5000);
   };
+
   useEffect(() => {
     nextCard();
   });
